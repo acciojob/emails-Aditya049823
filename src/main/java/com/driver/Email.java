445173b7fc78982711f6ded86857ae05a8251a02
Aditya <adityaharde05@gmail.com.com>
2023-02-20 12:06:57 +0530

    First Commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,11 +1,13 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Email {
 
     private String emailId;
     private String password;
 
-    public Email(String emailId){
+    public Email(String emailId) {
         this.emailId = emailId;
         this.password = "Accio@123";
     }
@@ -18,12 +20,60 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+    public void changePassword(String oldPassword, String newPassword) {
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword.equals(password))
+        {
+            if(isValid(newPassword))
+            {
+                this.password=newPassword;
+                System.out.println("Password changed sucessfully");
+            }
+            else
+            {
+                System.out.println("Invalid Password");
+            }
+        }
+        else
+        {
+            System.out.println("Password is incorrect");
+        }
+    }
+    private Boolean isValid(String newPassword)
+    {
+        Boolean isUpper=false;
+        Boolean isLower=false;
+        Boolean isDigit=false;
+        Boolean isSpecial=false;
+        int len=newPassword.length();
+
+        if(len<8)
+        {
+            return false;
+        }
+
+        for(int i=0;i<newPassword.length();i++) {
+            char ch = newPassword.charAt(i);
+            if ((ch >= 'A') && (ch <= 'Z')) {
+                isUpper = true;
+            } else if ((ch >= 'a') && (ch >= 'z')) {
+                isLower = true;
+            } else if ((ch >= '0') && (ch <= '9')) {
+                isDigit = true;
+            } else {
+                isSpecial = true;
+            }
+        }
+            if(isUpper && isLower && isDigit && isSpecial)
+            {
+                return true;
+            }
+            return false;
     }
 }
+
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -2,6 +2,7 @@ package com.driver;
 
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.text.DateFormat;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,11 +13,15 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-
+        super(emailId,Integer.MAX_VALUE);
+        this.calendar=new ArrayList<>();
     }
+
+
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
+        calendar.add(meeting);
 
     }
 
@@ -25,6 +30,26 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-
+        ArrayList<Pair<LocalTime,Integer>>endTym=new ArrayList<>();
+        for(int i=0;i<calendar.size();i++)
+        {
+            endTym.add(Pair.of(calendar.get(i).getEndTime(),i));
+        }
+        Collections.sort(endTym);
+        LocalTime tym_limit=endTym.get(0).getLeft();
+        int count=0;
+        if(!endTym.isEmpty())
+        {
+            count++;
+        }
+        for(int i=0;i<endTym.size();i++)
+        {
+            if(calendar.get(endTym.get(i).getRight()).getStartTime().compareTo(tym_limit)>0)
+            {
+                count++;
+                tym_limit=endTym.get(i).getLeft();
+            }
+        }
+        return count;
     }
 }
